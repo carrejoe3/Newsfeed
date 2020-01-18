d2a4b58d8dfc17277c45bcd013159a11220be173
Joe <carre.joe3@gmail.com>
2020-01-18 19:14:17 +0000

    added state value and mutations for articles

--- newsfeed/src/store/index.js
+++ newsfeed/src/store/index.js
@@ -5,8 +5,12 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    articles: null
   },
   mutations: {
+    updateArticles (state, articles) {
+      state.articles = articles
+    }
   },
   actions: {
   },
