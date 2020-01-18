0c7ee9481a975319d1b0a4d8c17ca6f52dcc30c4
Joe <carre.joe3@gmail.com>
2020-01-18 11:50:34 +0000

    Removed default vue project elements
    -Renamed 'HelloWorld' component to 'Newsfeed'
    -Removed default vue content
    -Removed 'About' view
    -Updated router

--- newsfeed/src/views/Home.vue
+++ newsfeed/src/views/Home.vue
@@ -1,18 +1,17 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-container class="home">
+    <NewsFeed/>
+  </v-container>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import NewsFeed from '@/components/NewsFeed.vue'
 
 export default {
   name: 'home',
   components: {
-    HelloWorld
+    NewsFeed
   }
 }
 </script>
