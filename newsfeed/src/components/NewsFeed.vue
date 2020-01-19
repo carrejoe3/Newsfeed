ec9f33df85210d4ec639a080ebfeefa192e5fb7d
Joe <carre.joe3@gmail.com>
2020-01-19 14:16:47 +0000

    Added a card element to the newsfeed component and filled it out with article data

--- newsfeed/src/components/NewsFeed.vue
+++ newsfeed/src/components/NewsFeed.vue
@@ -1,10 +1,35 @@
 <template>
-  <v-container></v-container>
+  <v-container>
+    <v-card class="mx-auto my-12" v-for="(article, index) in articles" :key="index">
+      <v-img :src="article.urlToImage"></v-img>
+      <v-card-title id="title">{{ article.title }}</v-card-title>
+      <v-card-text>
+        <div class="my-4 subtitle-1 black--text">{{ article.publishedAt }}</div>
+        <div>{{ article.description }}</div>
+      </v-card-text>
+      <v-divider class="mx-4"></v-divider>
+      <v-card-title>Tonight's availability</v-card-title>
+      <v-card-actions>
+        <v-btn color="deep-purple accent-4" text>Reserve</v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-container>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
-  data: () => ({})
+  name: 'NewsFeed',
+  data: () => ({}),
+  computed: {
+    articles () {
+      return this.$store.state.articles
+    }
+  }
 }
 </script>
+
+<style lang="scss" scoped>
+#title {
+  word-break: normal;
+}
+</style>
