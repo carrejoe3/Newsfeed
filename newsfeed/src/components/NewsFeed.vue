94612d6fd25129725d797030ee3756d2035bf38d
Joe <carre.joe3@gmail.com>
2020-01-19 14:40:07 +0000

    installed moment.js to format article dates/ removed unneeded content from article cards

--- newsfeed/src/components/NewsFeed.vue
+++ newsfeed/src/components/NewsFeed.vue
@@ -3,14 +3,15 @@
     <v-card class="mx-auto my-12" v-for="(article, index) in articles" :key="index">
       <v-img :src="article.urlToImage"></v-img>
       <v-card-title id="title">{{ article.title }}</v-card-title>
+      <v-divider class="mx-4"></v-divider>
       <v-card-text>
-        <div class="my-4 subtitle-1 black--text">{{ article.publishedAt }}</div>
+        <div class="my-4 subtitle-1 black--text">{{ article.publishedAt | formatDateTime }}</div>
         <div>{{ article.description }}</div>
       </v-card-text>
-      <v-divider class="mx-4"></v-divider>
-      <v-card-title>Tonight's availability</v-card-title>
       <v-card-actions>
-        <v-btn color="deep-purple accent-4" text>Reserve</v-btn>
+        <a :href="article.url" target="_blank">
+          <v-btn color="primary accent-4" text>Read</v-btn>
+        </a>
       </v-card-actions>
     </v-card>
   </v-container>
--- newsfeed/src/main.js
+++ newsfeed/src/main.js
@@ -3,8 +3,13 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import vuetify from './plugins/vuetify'
+import moment from 'moment'
 
 Vue.config.productionTip = false
+
+Vue.filter('formatDateTime', (date) => {
+  return moment.utc(date).local().format('dddd, MMMM Do YYYY, h:mm:ss a')
+})
 
 new Vue({
   router,
